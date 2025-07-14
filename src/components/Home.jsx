44cc4a130d0e84b587b24b204629ce505b862cf9
hoshikwyw <khaingwutyiwin1712@gmail.com>
2025-07-14 13:02:39 +0630

    Added Home page content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HomeWindow from '../common/HomeWindow'
 
 const Home = () => {
     return (
         <div className='contentContainer'>
-            
+            <HomeWindow />
         </div>
     )
 }
